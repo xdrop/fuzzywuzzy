523cfbd67b838c0c00d65ddbe918d9903c6e4504
xdrop <pastosdev@gmail.com>
2016-09-11 00:31:14 +0300

    Add processAndSort and tokenSort

--- src/com/xdrop/fuzzywuzzy/FuzzySearch.java
+++ src/com/xdrop/fuzzywuzzy/FuzzySearch.java
@@ -2,21 +2,39 @@ package com.xdrop.fuzzywuzzy;
 
 import com.xdrop.diffutils.DiffUtils;
 import com.xdrop.diffutils.structs.MatchingBlock;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @SuppressWarnings("WeakerAccess")
 public class FuzzySearch {
 
-    public static double ratio(String s1, String s2) {
 
-        return Math.round(100 * DiffUtils.getRatio(s1, s2));
+    /**
+     * Calculates a Levenshtein simple ratio between the strings.
+     * This is indicates a measure of similarity
+     *
+     * @param s1 Input string
+     * @param s2 Input string
+     * @return The simple ratio
+     */
+    public static int ratio(String s1, String s2) {
+
+        return (int) Math.round(100 * DiffUtils.getRatio(s1, s2));
 
     }
 
-    public static double partialRatio(String s1, String s2) {
+    /**
+     * Returns a partial ratio.
+     *
+     * @param s1 Input string
+     * @param s2 Input string
+     * @return The partial ratio
+     */
+    public static int partialRatio(String s1, String s2) {
 
         String shorter;
         String longer;
@@ -56,9 +74,33 @@ public class FuzzySearch {
 
         }
 
-        return Math.round(100 * Collections.max(scores));
+        return (int) Math.round(100 * Collections.max(scores));
 
     }
+
+    private String processAndSort(String in) {
+
+        in = Utils.processString(in, false);
+        String[] wordsArray = in.split("\\s+");
+
+        List<String> words = Arrays.asList(wordsArray);
+        Collections.sort(words);
+
+        String joined = StringUtils.join(words, " ");
+
+        return joined.trim();
+
+    }
+
+    private int tokenSort(String s1, String s2, boolean partial) {
+
+        String sorted1 = processAndSort(s1);
+        String sorted2 = processAndSort(s2);
+
+        return partial ? partialRatio(sorted1, sorted2) : ratio(sorted1, sorted2);
+
+    }
+
 
 
 }
